89cc20cce3b5e34230ee77469b386d89f60b3949
IftekharAlamMithu <iftekharalammithu23@gmail.com>
2024-10-16 15:59:28 +0600

    "Updated package.json, added TonConnectUIProvider, and configured tailwindcss in vite.config.js"

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <TonConnectUIProvider manifestUrl="https://<YOUR_APP_URL>/tonconnect-manifest.json">
+      <App />
+    </TonConnectUIProvider>
+  </StrictMode>
+);
